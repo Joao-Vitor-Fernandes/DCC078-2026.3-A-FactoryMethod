e703c14cbdabe981cea6978f15c522971103f3f7
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2026-09-04 20:13:03 -0300

    adicionar segundo tipo de pagamento, pagamento via Cartão

---
+++ src/main/java/padroescriacao/factorymethod/PagamentoCartao.java
@@ -0,0 +1,11 @@
+package padroescriacao.factorymethod;
+
+public class PagamentoCartao implements IPagamento {
+    public String autorizar() {
+        return "Pagamento via Cartão autorizado";
+    }
+
+    public String estornar() {
+        return "Pagamento via Cartão estornado";
+    }
+}
