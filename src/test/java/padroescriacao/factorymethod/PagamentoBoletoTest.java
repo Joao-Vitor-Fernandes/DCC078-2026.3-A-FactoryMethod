dee1f8d0af1d7d3211322f6fe49d51ed871fa1b2
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2026-09-05 00:54:42 -0300

    adicionar teste de PagamentoFactory e ajustar teste de Boleto

--- src/test/java/padroescriacao/factorymethod/PagamentoBoletoTest.java
+++ src/test/java/padroescriacao/factorymethod/PagamentoBoletoTest.java
@@ -9,12 +9,12 @@ class PagamentoBoletoTest {
     @Test
     void deveAutorizarBoleto() {
         IPagamento pagamento = PagamentoFactory.obterPagamento("Boleto");
-        assertEquals("Boleto gerado com sucesso", pagamento.executar());
+        assertEquals("Boleto gerado com sucesso", pagamento.autorizar());
     }
 
     @Test
     void deveEstornarBoleto() {
         IPagamento pagamento = PagamentoFactory.obterPagamento("Boleto");
-        assertEquals("Pagamento via Boleto cancelado", pagamento.cancelar());
+        assertEquals("Pagamento via Boleto cancelado", pagamento.estornar());
     }
 }
---
+++ src/test/java/padroescriacao/factorymethod/PagamentoFactoryTest.java
@@ -0,0 +1,28 @@
+package padroescriacao.factorymethod;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PagamentoFactoryTest {
+
+    @Test
+    void deveRetornarExcecaoParaPagamentoInexistente() {
+        try {
+            IPagamento pagamento = PagamentoFactory.obterPagamento("DinheiroFisico");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Pagamento inexistente", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarExcecaoParaPagamentoInvalido() {
+        try {
+            IPagamento pagamento = PagamentoFactory.obterPagamento("Cartao");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Pagamento inválido", e.getMessage());
+        }
+    }
+}
